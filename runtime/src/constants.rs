3569413fe294224803173ddf7155b728f41154dc
HuiFeng Tang <1290017556@qq.com>
2022-01-21 13:54:44 +0800

    fix some detail (#5)

--- runtime/src/constants.rs
+++ runtime/src/constants.rs
@@ -3,17 +3,15 @@ pub mod currency {
 
 	pub const PL: Balance = 10_000_000_000;
 	pub const WEI: Balance = 1;
-    // pub const UNITS: Balance = 1_000_000_000_000_000_000;
 	pub const KILOWEI: Balance = 1_000;
 	pub const MEGAWEI: Balance = 1_000_000;
 	pub const GIGAWEI: Balance = 1_000_000_000;
 
-	pub const MILLICENTS: Balance = 10_000_000_000_000;
-	pub const CENTS: Balance = 1_000 * MILLICENTS; // assume this is worth about a cent.
-	pub const DOLLARS: Balance = 100 * CENTS;
+	pub const DOLLARS: Balance = PL; // 10_000_000_000
+	pub const CENTS: Balance = DOLLARS / 100; // 100_000_000
+	pub const MILLICENTS: Balance = CENTS / 1_000; // 100_000
 
 	pub const fn deposit(items: u32, bytes: u32) -> Balance {
 		items as Balance * 15 * CENTS + (bytes as Balance) * 6 * CENTS
 	}
 }
-
